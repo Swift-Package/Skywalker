fcf69272871c7f0894337a450691649a6208e979
杨俊艺 <1209032479@qq.com>
2022-06-11 20:31:51 +0800

    添加判断用户界面是不是竖屏的工具类

---
+++ Sources/Skywalker/Tools/UITool/UI.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by 杨俊艺 on 2022/6/11.
+//
+
+import UIKit
+
+@objcMembers
+/// 用来判断设备及用户界面特征
+public class UI: NSObject {
+    public static let shared = UI.init()
+    
+    public var isPortrait: Bool {
+        UIApplication.shared.connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .compactMap { $0 }
+            .first!.interfaceOrientation.isPortrait
+    }
+    
+    public var isiPad: Bool { UIDevice.current.userInterfaceIdiom == .pad }
+    
+}
+
