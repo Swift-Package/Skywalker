7f6eeeb2f8a51ac21f75e49535dbde7f1241640c
Tim 杨俊艺 <tim_yang@intretech.com>
2022-11-30 14:02:19 +0800

    Almofire网络请求库已经有这个扩展导致冲突

--- Sources/Skywalker/DispatchDevelop/DispatchQueue/DispatchQueueDelay.swift
+++ Sources/Skywalker/DispatchDevelop/DispatchQueue/DispatchQueueDelay.swift
@@ -10,11 +10,4 @@ import Foundation
 @objc
 public extension DispatchQueue {
     
-    /// 几秒后在主线程上回调Closure
-    /// - Parameters:
-    ///   - delay: 延迟时间/秒
-    ///   - closure: 回调闭包
-    class func delay(_ delay: Double, closure: @escaping () -> ()) {
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + delay, execute: closure)
-    }
 }
